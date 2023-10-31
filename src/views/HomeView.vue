c5b162da277ccb51b15f918e322f0e6c6663aa51
N-Mehroj <72346597+N-Mehroj@users.noreply.github.com>
2023-10-31 18:58:54 +0500

    Update HomeView.vue
    sss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,7 +31,8 @@ onMounted(() => {
   googleOneTap()
     .then((response) => {
       // This promise is resolved when user selects an account from the the One Tap prompt
-      console.log("Handle the response", response)
+       const userData = decodeCredential(response.credential)
+       console.log("Handle the userData", userData)
     })
     .catch((error) => {
       console.log("Handle the error", error)
